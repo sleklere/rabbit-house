18014dddbc70a96c6f45ddc1187933f41813ab70
sleklere <santiagoleklere@gmail.com>
2024-02-15 16:09:04 -0300

    working for you page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import logo from "./assets/rabbit_logo.png";
+import scribble1 from "./assets/2.png";
+import scribble2 from "./assets/4.png";
+
 function App() {
-  return <></>;
+  return (
+    <div className="working-for-you">
+      <img src={scribble1} className="scribble-1" />
+      <img src={scribble2} className="scribble-2" />
+      <img src={logo} className="logo" />
+      <h1>WORKING FOR YOU</h1>
+    </div>
+  );
 }
 
 export default App;
