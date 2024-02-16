b37446a35822cc119aca1c3d6fbb9b28ece69d95
sleklere <santiagoleklere@gmail.com>
2024-02-15 23:40:19 -0300

    working for you done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import logo from "./assets/rabbit_logo.png";
 import scribble1 from "./assets/2.png";
 import scribble2 from "./assets/4.png";
+import fb from "./assets/fb_icon.png";
+import ig from "./assets/ig_icon.png";
 
 function App() {
   return (
@@ -9,6 +11,17 @@ function App() {
       <img src={scribble2} className="scribble-2" />
       <img src={logo} className="logo" />
       <h1>WORKING FOR YOU</h1>
+      <div className="icons">
+        <a href="https://www.facebook.com/rabbithousemx" target="_blank">
+          <img src={fb} className="icon-fb" />
+        </a>
+        <a
+          href="https://www.instagram.com/rabbithousemx?igsh=MXVuN2twMmh3cTRubw=="
+          target="_blank"
+        >
+          <img src={ig} className="icon-ig" />
+        </a>
+      </div>
     </div>
   );
 }
